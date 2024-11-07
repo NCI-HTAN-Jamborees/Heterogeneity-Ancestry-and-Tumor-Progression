547ad94e7f249a4f28f8909e609b2fa3f946dcc7
jiaying2508 <38696658+jiaying2508@users.noreply.github.com>
2024-11-07 12:58:15 -0500

    Update non-epithelial_set_analysis.R

--- scripts/non-epithelial_set_analysis.R
+++ scripts/non-epithelial_set_analysis.R
@@ -73,19 +73,29 @@ summary_tumor_race = as.data.frame(table(m_addbiospecimens$Tumor_Tissue_Type, m_
     Race = Var2
   )
 write.table(summary_tumor_race,"results/non-epithelial/summary_tumor_race_before_filtering.tab",sep="\t",row.names = F, quote=F)
+
 m_needed = m_addbiospecimens %>%
   filter(self_reported_ethnicity == "European" | self_reported_ethnicity == "African American") %>%
-  filter(Tumor_Tissue_Type=="Normal" | Tumor_Tissue_Type=="Premalignant")
+  filter(Tumor_Tissue_Type=="Normal" | Tumor_Tissue_Type=="Premalignant") %>%
+  filter(Tumor_Type=="NL" | Tumor_Type=="SSL" | Tumor_Type=="TA")
 m_needed$self_reported_ethnicity = as.character(m_needed$self_reported_ethnicity)
 m_needed$self_reported_ethnicity = factor(m_needed$self_reported_ethnicity)
+
 colnames(m_addbiospecimens)
+m_needed = m_needed %>%
+  mutate(age = gsub("-year-old stage","",development_stage)) %>%
+  mutate(age_group = ifelse(age >60,">60","<=60"))
+table(m_needed$age)
+
+table(m_needed$Tumor_Type, m_needed$self_reported_ethnicity)
 
 cells_needed = m_needed$cell_barcode
-m_need_for_metadata = m_needed[,c("cell_barcode","Tumor_Tissue_Type")]
+
+m_need_for_metadata = m_needed[,c("cell_barcode","Tumor_Tissue_Type","age_group")]
 rownames(m_need_for_metadata) = m_need_for_metadata$cell_barcode
 so=object
 so = subset(so,cells=cells_needed)
-so
+# so
 so = AddMetaData(so,m_need_for_metadata)
 summary_tumor_race_after = as.data.frame(table(m_needed$Tumor_Tissue_Type, m_needed$self_reported_ethnicity)) %>%
   pivot_wider(names_from ="Var1", values_from = "Freq") %>%
@@ -93,8 +103,86 @@ summary_tumor_race_after = as.data.frame(table(m_needed$Tumor_Tissue_Type, m_nee
     Race = Var2
   )
 summary_tumor_race_after
-write.table(summary_tumor_race_after,"summary_tumor_race_before_filtering.tab",sep="\t",row.names = F, quote=F)
+write.table(summary_tumor_race_after,"results/non-epithelial/summary_tumor_race_before_filtering.tab",sep="\t",row.names = F, quote=F)
+
+saveRDS(so,"CellxGene/Filtered_no_Epithelial.rds")
+
+cell.size = 0.1
+resolution = 0.2
+reduction_name = "umap"
+#p1 = DimPlot(so,reduction="umap.integrated.rpca", pt.size =0.1, group.by = c("condition"))
+p1 = DimPlot(so,reduction=reduction_name, pt.size =0.1, group.by = c("self_reported_ethnicity"))
+p2 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Tumor_Tissue_Type")
+#p2 = DimPlot(so, reduction=reduction_name,group.by = "rna_clusters")
+p3 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Cell_Type")
+p4 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "sex")
+# p5 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Polyp_Type")
+p6 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "age_group")
+# p7 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "Polyp_Type", group.by = "self_reported_ethnicity")
+#p1+p2+p3
+#p4+p5+p6
+
+p7
+ggsave(paste0 (umap_path,"/1.UMAP_splitPolyb_groupRace_so.pdf"), p7, width = 18, height = 5)
+
+combined_plot <- p1 + p2 + p3 + p4 +p5+p6
+# Arrange plots into a 2x3 grid
+combined_plot <- combined_plot + plot_layout(ncol = 3)
+plot(combined_plot) 
+# Save the combined plot as a PDF file
+ggsave(paste0 (umap_path,"/1.UMAP_so.pdf"), combined_plot, width = 18, height = 10)
 
 
 
+m_so = so@meta.data
+table(m_so$development_stage)
+summary_polyb_race_after = as.data.frame(table(m_so$Polyp_Type, m_so$self_reported_ethnicity)) %>%
+  pivot_wider(names_from ="Var1", values_from = "Freq") %>%
+  rename(
+    Race = Var2
+  )
+summary_polyb_race_after
+
+
+## subset based on Polyp_Type
+m_final = m_so %>%
+  filter(Polyp_Type != "TV" & Polyp_Type != "UNC")
+cells_needed_final = m_final$cell_barcode
+so_final = subset(so,cells = cells_needed_final)
+
+
+
+#p1 = DimPlot(so,reduction="umap.integrated.rpca", pt.size =0.1, group.by = c("condition"))
+p1 = DimPlot(so_final,reduction=reduction_name, pt.size =0.1, group.by = c("self_reported_ethnicity"))
+p2 = DimPlot(so_final, reduction=reduction_name,pt.size =0.1, group.by = "Tumor_Tissue_Type")
+#p2 = DimPlot(so, reduction=reduction_name,group.by = "rna_clusters")
+p3 = DimPlot(so_final, reduction=reduction_name,pt.size =0.1, group.by = "Cell_Type")
+p4 = DimPlot(so_final, reduction=reduction_name,pt.size =0.1, group.by = "sex")
+p5 = DimPlot(so_final, reduction=reduction_name,pt.size =0.1, group.by = "Polyp_Type")
+p6 = DimPlot(so_final, reduction=reduction_name,pt.size =0.1, group.by = "age_group")
+p7 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "Polyp_Type", group.by = "self_reported_ethnicity")
+p8 = DimPlot(so_final, reduction=reduction_name,pt.size =0.1, split.by = "Polyp_Type", group.by = "self_reported_ethnicity")
+
+p1+p2+p3
+p4+p5+p6
+
+p8
+ggsave(paste0 (umap_path,"/1.UMAP_splitPolyb_groupRace_so_NLSSLTA.pdf"), p8, width = 12, height = 5)
+
+combined_plot <- p1 + p2 + p3 + p4 +p5+p6
+# Arrange plots into a 2x3 grid
+combined_plot <- combined_plot + plot_layout(ncol = 3)
+plot(combined_plot) 
+# Save the combined plot as a PDF file
+ggsave(paste0 (umap_path,"/1.UMAP_so_NLSSLTA.pdf"), combined_plot, width = 18, height = 10)
+
+
+
+
+
+
+
+
+
+sessionInfo()
 
