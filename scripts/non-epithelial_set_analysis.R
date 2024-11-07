a548b19f518af9d31df7e4e24cd580a701360a7e
jiaying2508 <38696658+jiaying2508@users.noreply.github.com>
2024-11-07 14:09:01 -0500

    figures

--- scripts/non-epithelial_set_analysis.R
+++ scripts/non-epithelial_set_analysis.R
@@ -38,34 +38,18 @@ reduction_name = "umap"
 # Add cell carcode to meta data
 m = object@meta.data %>%
   rownames_to_column(var ="cell_barcode")
-# rownames(m) = rownames(object@meta.data)
-# colnames(m)
-# rownames(m)[1000:1005]
-# m$cell_barcode[1000:1005]
-
-# table(m$self_reported_ethnicity)
-# table(m$disease)
-# m$`HTAN Specimen ID`
-# getwd()
 
 # Load metadata file with biospecimens info
 biospecimens_df= read.table("metadata/biospecimens.tsv",header=T,sep="\t")
-#colnames(biospacemens_df)
 dim(biospecimens_df)
-#biospecimens_df_need = biospecimens_df[,c("HTAN.Biospecimen.ID","HTAN.Parent.ID", "Collection.Days.from.Index", "Storage.Method", "Processing.Days.from.Index", "Preservation.Method", "Tumor.Tissue.Type")]
-#colnames(biospecimens_df_need) = c("HTAN_Biospecimen_ID","HTAN_Participant_ID", "Collection_Days_from_Index", "Storage_Method", "Processing_Days_from_Index", "Preservation_Method", "Tumor_Tissue_Type")
 biospecimens_df_need = biospecimens_df[,c("HTAN.Biospecimen.ID","Tumor.Tissue.Type")]
 colnames(biospecimens_df_need) = c("HTAN_Biospecimen_ID","Tumor_Tissue_Type")
 biospecimens_df_need
 cat("There are ",length(unique(biospecimens_df_need$HTAN_Biospecimen_ID)), " biospecimens from  Vanderbilt Colon Molecular Atlas Project")
-#head(biospecimens_df_need)
 as.data.frame(table(biospecimens_df_need$Tumor_Tissue_Type))
-#case_df_need = case_df[,c("HTAN.Participant.ID","Ethnicity","Gender","Race")] #,"Age.at.Diagnosis..years.","Primary.Diagnosis","Year.of.Diagnosis")]
-#colnames(case_df_need) =c("HTAN_Participant_ID","Ethnicity","Gender","Race")
 
 m_addbiospecimens = m %>%
   left_join(biospecimens_df_need, by= c("HTAN Specimen ID"  = "HTAN_Biospecimen_ID"))
-# rownames(m_addbiospecimens) = rownames(m)
 
 summary_tumor_race = as.data.frame(table(m_addbiospecimens$Tumor_Tissue_Type, m_addbiospecimens$self_reported_ethnicity)) %>%
   pivot_wider(names_from ="Var1", values_from = "Freq") %>%
@@ -81,7 +65,7 @@ m_needed = m_addbiospecimens %>%
 m_needed$self_reported_ethnicity = as.character(m_needed$self_reported_ethnicity)
 m_needed$self_reported_ethnicity = factor(m_needed$self_reported_ethnicity)
 
-colnames(m_addbiospecimens)
+# colnames(m_addbiospecimens)
 m_needed = m_needed %>%
   mutate(age = gsub("-year-old stage","",development_stage)) %>%
   mutate(age_group = ifelse(age >60,">60","<=60"))
@@ -93,9 +77,9 @@ cells_needed = m_needed$cell_barcode
 
 m_need_for_metadata = m_needed[,c("cell_barcode","Tumor_Tissue_Type","age_group")]
 rownames(m_need_for_metadata) = m_need_for_metadata$cell_barcode
+
 so=object
 so = subset(so,cells=cells_needed)
-# so
 so = AddMetaData(so,m_need_for_metadata)
 summary_tumor_race_after = as.data.frame(table(m_needed$Tumor_Tissue_Type, m_needed$self_reported_ethnicity)) %>%
   pivot_wider(names_from ="Var1", values_from = "Freq") %>%
@@ -114,16 +98,54 @@ reduction_name = "umap"
 p1 = DimPlot(so,reduction=reduction_name, pt.size =0.1, group.by = c("self_reported_ethnicity"))
 p2 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Tumor_Tissue_Type")
 #p2 = DimPlot(so, reduction=reduction_name,group.by = "rna_clusters")
-p3 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Cell_Type")
+p3 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by="self_reported_ethnicity", group.by = "Cell_Type")
 p4 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "sex")
-# p5 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Polyp_Type")
+p5 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "Tumor_Type")
 p6 = DimPlot(so, reduction=reduction_name,pt.size =0.1, group.by = "age_group")
-# p7 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "Polyp_Type", group.by = "self_reported_ethnicity")
+p7 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "Tumor_Type", group.by = "self_reported_ethnicity")
 #p1+p2+p3
 #p4+p5+p6
 
+
+p9 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "self_reported_ethnicity", group.by = "Cell_Type")
+
+m_so = so@meta.data
+
+long_data_cluster <- m_so[,c("self_reported_ethnicity","Cell_Type")] %>% 
+  group_by(self_reported_ethnicity,Cell_Type) %>%
+  summarise(count=n()) %>%
+  mutate(percentage = count / sum(count) * 100)
+
+long_data_cluster
+bar_cluster =ggplot(long_data_cluster, aes(x = self_reported_ethnicity, y = percentage, fill = Cell_Type)) +
+  geom_bar(stat = "identity", position = "fill") +
+  geom_text(
+    data=filter(long_data_cluster,percentage>1),
+    aes(label = count), position = position_fill(vjust = 0.5),
+    color = "white") +
+  labs(title = "Percentage Bar Plot", x = "SampleID", y = "Percentage") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) + 
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.title = element_text(size = 20),  # Increase legend title size
+        legend.text = element_text(size = 15)) +
+  guides(fill = guide_legend(override.aes = list(size = 8)))
+bar_cluster
+ggsave(plot = bar_cluster,height =4,width =5, filename = paste0(umap_path, "/barplot_cluster.pdf"))
+
+
+p10 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "Tumor_Type", group.by = "Cell_Type")
+
+so$Tumor_Type_Race <- paste(so$Tumor_Type, so$self_reported_ethnicity, sep = "_")
+
+p11 = DimPlot(so, reduction=reduction_name,pt.size =0.1, split.by = "Tumor_Type_Race", ncol = 2, group.by = "Cell_Type")
+
+ggsave(paste0 (umap_path,"/UMAP_splitRaceTumorType_groupCellType_so.pdf"), p11, width = 12, height = 5)
+
+##################
+
 p1
-ggsave(paste0 (umap_path,"/1.UMAP_splitPolyb_groupRace_so.pdf"), p7, width = 18, height = 5)
+ggsave(paste0 (umap_path,"/UMAP_splitPolyb_groupRace_so.pdf"), p7, width = 18, height = 5)
 
 combined_plot <- p1 + p2 + p3 + p4 +p5+p6
 # Arrange plots into a 2x3 grid
@@ -133,8 +155,6 @@ plot(combined_plot)
 ggsave(paste0 (umap_path,"/1.UMAP_so.pdf"), combined_plot, width = 18, height = 10)
 
 
-
-m_so = so@meta.data
 table(m_so$development_stage)
 summary_polyb_race_after = as.data.frame(table(m_so$Polyp_Type, m_so$self_reported_ethnicity)) %>%
   pivot_wider(names_from ="Var1", values_from = "Freq") %>%
